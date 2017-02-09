33fe292ecc0c20b69aa54d5b98bbef9e148b6a63
Joacim Åström <joacim.astrom@gmail.com>
2017-02-09 14:28:58 +0100

    tests a few matches from test-set

--- find_match.py
+++ find_match.py
@@ -1,10 +1,9 @@
-def find_match(model, product_a_id):
-    max_score_and_product_id = (0, None)
+def find_match(model, product_a_id, range):
+	scores = []
 
-    for product_b in model.products:
-        new_score = model.score(product_a_id, product_b)
-
-        if new_score > max_score_and_product_id[0]:
-            max_score_and_product_id = (new_score, product_b)
-
-    return max_score_and_product_id
+	for product_b in model.products:
+		new_score = model.score(product_a_id, product_b)
+		scores.append((new_score, product_b))
+		scores.sort(key=lambda tup: -tup[0])
+		scores = scores[:range]
+	return scores
--- index.py
+++ index.py
@@ -13,20 +13,32 @@ def train_and_evaluate_simple_model():
     matches = match.load_matches("./data/matches.csv")
     (train, test) = split.split_train_test(matches)
 
+
     model = simple_model.SimpleModel(products, attributes)
     model.train(train)
 
-    return validate.validate_model(model, test)
-    # print(find_match.find_match(model, 31140))
-    # print(find_match.find_match(model, 33795))
-    # print(find_match.find_match(model, 49031))
+    test_matches = []
+    for _ in range(10):
+        test_matches.append(test.pop())
+
+    precision = 10
+    for original, new in test_matches:
+        if new in [x[1] for x in find_match.find_match(model, original, precision)]:
+            print(str(original) + " found correct match " + str(new) + " within top " + str(precision) + " matches") 
+        else:
+            print("Nope")
+
+    #return validate.validate_model(model, test)
+    #print(find_match.find_match(model, 31140))
+    #print(find_match.find_match(model, 33795, 3))
+    #print(find_match.find_match(model, 49031))
 
 
 if __name__ == '__main__':
-    total = 0
-    for i in range(15):
-        score = train_and_evaluate_simple_model()
-        print(str(i) + " - Average: " + str(score))
-        total += score
-    print(total / 15)
+    # total = 0
+    # for i in range(15):
+    score = train_and_evaluate_simple_model()
+    #     print(str(i) + " - Average: " + str(score))
+    #     total += score
+    # print(total / 15)
 
