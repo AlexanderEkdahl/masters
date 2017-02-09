5d739c3c48901d47c685ca73b824a44455d7c11c
Joacim Åström <joacim.astrom@gmail.com>
2017-02-09 14:38:09 +0100

    refactored find_match

--- find_match.py
+++ find_match.py
@@ -1,9 +1,10 @@
 def find_match(model, product_a_id, range):
-	scores = []
+	top_matches = []
 
 	for product_b in model.products:
 		new_score = model.score(product_a_id, product_b)
-		scores.append((new_score, product_b))
-		scores.sort(key=lambda tup: -tup[0])
-		scores = scores[:range]
-	return scores
+		if new_score[1] > top_matches[-1][1]
+			top_matches.append((new_score, product_b))
+			top_matches.sort(key=lambda tup: -tup[0])
+			top_matches = top_matches[:range]
+	return top_matches
