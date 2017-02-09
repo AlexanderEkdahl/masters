d3b28a89ac2943447815b7e041a2042f7fe5196d
Joacim Åström <joacim.astrom@gmail.com>
2017-02-09 14:38:59 +0100

    tiny fix

--- find_match.py
+++ find_match.py
@@ -3,7 +3,7 @@ def find_match(model, product_a_id, range):
 
 	for product_b in model.products:
 		new_score = model.score(product_a_id, product_b)
-		if new_score[1] > top_matches[-1][1]
+		if new_score > top_matches[-1][1]
 			top_matches.append((new_score, product_b))
 			top_matches.sort(key=lambda tup: -tup[0])
 			top_matches = top_matches[:range]
