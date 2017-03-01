c9ad821c42196fc7473b7cf33af54f92efd28e01
Joacim Åström <joacim.astrom@gmail.com>
2017-03-01 16:26:47 +0100

    match prep

--- sql/match_prep.rb
+++ sql/match_prep.rb
@@ -1,27 +1,30 @@
 require 'csv'
+require 'set'
 
 
-matches = []
+matches = {}
 products = []
 
+# dict = {}
 
-CSV.foreach("../data/products.csv") do |row|
-  products << row[0]
+
+CSV.foreach("../data/subset_no_empty_columns.csv") do |row|
+ products << row[0]
+ # id = row[0]
+ # row.shift(2)
+ # dict[id] = row 
 end
 
 CSV.foreach("../data/matches.csv") do |row|
-  matches << [row[0], row[1]] if products.include?(row[0]) && products.include?(row[1])
+  matches[row[1]] = Set.new if !matches.has_key?(row[1])
+  matches[row[1]].add(row[0]) if products.include?(row[0])
 end
 
 
-matches.uniq!
-
-
-
-
-# CSV.open("workshop.csv", "wb") do |csv|
-# 	csv << ["id", "name", attribute_names].flatten
-# 	product_id.each.with_index do |id, i|
-# 		csv << [id, product_names[i], attributes[i]].flatten
-# 	end
-# end
+CSV.open("../data/filtered_matches.csv", "wb") do |csv|
+	matches.each do |k, v|
+		if v.count > 0
+			v.each { |x| csv << [x, k].flatten}
+		end
+	end
+end
